c940044e70b6d6bd5fd88f081977ece967f6b522
honghyungmin <hymin06@gmail.com>
2023-01-05 01:12:20 +0900

    Co
    Co

---
+++ Assets/01_Script/Core/Define.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Define : MonoBehaviour
+{
+    private static Camera _mainCam = null;
+
+    public static Camera MainCam
+    {
+        get
+        {
+            if (_mainCam == null)
+                _mainCam = Camera.main;
+            return _mainCam;
+        }
+
+    }
+    private static CinemachineVirtualCamera _cmVCam = null;
+    public static CinemachineVirtualCamera VCam
+    {
+        get
+        {
+            if (_cmVCam == null)
+                _cmVCam = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+            return _cmVCam;
+        }
+    }
+        public static LayerMask Player = 1 << 6;
+        public static LayerMask Enemy = 1 << 7;
+        public static LayerMask Floor = 1 << 8;
+        public static LayerMask PassingFloor = 1 << 9;
+        public static LayerMask Weapon = 1 << 10;
+        public static LayerMask HolyWaterGlass = 1 << 11;
+
+}
